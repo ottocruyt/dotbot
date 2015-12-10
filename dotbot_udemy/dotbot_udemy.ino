a924b30045d3d99087fd4a5c72491b335a7c0b91
Steven Robyns <robynssteven@gmail.com>
2015-12-10 19:24:16 +0100

    Added stop function

--- dotbot_udemy/dotbot_udemy.ino
+++ dotbot_udemy/dotbot_udemy.ino
@@ -11,12 +11,19 @@ void forward()
   rightServo.writeMicroseconds(1500-power);
 }
 
+void stop()
+{
+  leftServo.writeMicroseconds(1500);
+  rightServo.writeMicroseconds(1500);
+}
 void setup()
 {
   leftServo.attach(6);
   rightServo.attach(5);
   
   forward();
+  delay(1000);
+  stop();
 }
 
 void loop()
