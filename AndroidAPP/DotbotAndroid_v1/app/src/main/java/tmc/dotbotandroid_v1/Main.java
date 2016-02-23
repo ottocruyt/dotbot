c653cfedfcfb5a9cdf73268400c50c6e1e992646
Steven Robyns <robynssteven@gmail.com>
2016-02-23 19:52:34 +0100

    Added comments in Main.java

--- AndroidAPP/DotbotAndroid_v1/app/src/main/java/tmc/dotbotandroid_v1/Main.java
+++ AndroidAPP/DotbotAndroid_v1/app/src/main/java/tmc/dotbotandroid_v1/Main.java
@@ -16,44 +16,54 @@ import android.view.WindowManager;
 
 public class Main extends AppCompatActivity implements SensorEventListener {
 
+    // Declaration sensor variables
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
 
+    // Initialize variable to check when sensor inputs have to be updated
+    private long lastUpdate = 0;
+
+    // Flags that give information whether a button is pressed or not
     private boolean startButtonPressed;
     private boolean connectButtonPressed;
 
-    private long lastUpdate = 0;
-
     @Override
+    // This method is called when the app is started.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //keep the screen on while in the app
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); // Functionality that keeps the screen on while being in the app
+
+        // Creation of sensor variables: a listener is created to keep track when the sensor values change
         senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
+        // Initialization of button flags
         startButtonPressed = false;
         connectButtonPressed = false;
     }
 
     @Override
+    // This method is called every time the sensor values change
     public void onSensorChanged(SensorEvent sensorEvent) {
         Sensor mySensor = sensorEvent.sensor;
 
         if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
+            // Call the current time so the sensor values are only updated after 100 ms.
             long curTime = System.currentTimeMillis();
-
             if ((curTime - lastUpdate) > 100) {
-                long diffTime = (curTime - lastUpdate);
                 lastUpdate = curTime;
                 float x = sensorEvent.values[0];
                 float y = sensorEvent.values[1];
                 float z = sensorEvent.values[2];
 
+                // Only the y- and z-axis are used to control the dotbot
+                // Once the new values are known, the Arduino input values need to be calculated
                 int result[] = calculateArduinoInputs(y,z);
 
+                // The new values need to be displayed in the GUI
                 refreshGUI(result[0], result[1], result [2], result [3]);
             }
         }
@@ -64,32 +74,25 @@ public class Main extends AppCompatActivity implements SensorEventListener {
 
     }
 
+    // Calculates the Arduino inputs based on the accelerometer sensor values
     public int[] calculateArduinoInputs(float y, float z) {
 
         int power = Math.round(z * 10);
         int steering = Math.round(y * 5);
-
         int motorLeft = power - steering;
         int motorRight = power + steering;
 
-        if (motorLeft < -100){
-            motorLeft = -100;
-        }
-        if (motorLeft > 100){
-            motorLeft = 100;
-        }
-        if (motorRight < -100){
-            motorRight = -100;
-        }
-        if (motorRight > 100){
-            motorRight = 100;
-        }
-        if (Math.abs(motorLeft) < 10) {
-            motorLeft = 0;
-        }
-        if (Math.abs(motorRight) < 10) {
-            motorRight = 0;
-        }
+        // Limitation to minimum and maximum values for servo motors
+        if (motorLeft < -100){motorLeft = -100;}
+        if (motorLeft > 100){motorLeft = 100;}
+        if (motorRight < -100){motorRight = -100;}
+        if (motorRight > 100){motorRight = 100;}
+
+        // Deadband to keep the dotbot steady when the value is close to zero
+        if (Math.abs(motorLeft) < 10) {motorLeft = 0;}
+        if (Math.abs(motorRight) < 10) {motorRight = 0;}
+
+        // Stop button functionality: the values are zero when the start button is not pressed
         if (!startButtonPressed){
             motorRight = 0;
             motorLeft = 0;
@@ -98,10 +101,11 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         return new int[]{motorLeft, motorRight, power, steering};
     }
 
+    // Refreshes the values that are displayed on the GUI screen
     public void refreshGUI(float motorLeft, float motorRight, float power, float steering) {
         TextView text = (TextView)findViewById(R.id.number_1);
 
-        //format to not show decimals
+        // Format so decimals are not shown
         DecimalFormat nd = new DecimalFormat("###");
         text.setText("Motor Left: " + nd.format(motorLeft));
         text = (TextView)findViewById(R.id.number_2);
@@ -112,10 +116,13 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         text.setText(nd.format(steering));
     }
 
+    // This function is called when the start button is clicked
     public void startButtonClick(View view) {
 
+        // Button object initialization
         final Button startButton = (Button) findViewById(R.id.button);
 
+        // Change of appearance when the start button is pressed
         if (startButtonPressed){
             startButton.setText("Start");
             startButton.setTextColor(ContextCompat.getColor(this, R.color.startColor));
@@ -131,11 +138,13 @@ public class Main extends AppCompatActivity implements SensorEventListener {
         }
 
     }
-
+    // This function is called when the connect button is clicked
     public void connectButtonClick(View view) {
 
+        // Button object initialization
         final Button connectButton = (Button) findViewById(R.id.button2);
 
+        // Change of appearance when the connect button is pressed
         if (connectButtonPressed){
             connectButton.setText("Connect");
             connectButtonPressed = false;
